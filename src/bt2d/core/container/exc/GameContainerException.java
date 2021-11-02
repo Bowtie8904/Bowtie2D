c09317e9cb32092cd0730f755afbd6e420007803
Bowtie8904 <30761922+Bowtie8904@users.noreply.github.com>
2021-11-02 21:51:47 +0100

    Added comments.

--- src/bt2d/core/container/exc/GameContainerException.java
+++ src/bt2d/core/container/exc/GameContainerException.java
@@ -12,6 +12,9 @@ public class GameContainerException extends RuntimeException
      * Creates a new exception with the given message.
      *
      * @param message The message of the exception.
+     *
+     * @author Lukas Hartwig
+     * @since 02.11.2021
      */
     public GameContainerException(String message)
     {
@@ -23,6 +26,9 @@ public class GameContainerException extends RuntimeException
      *
      * @param message The message of the exception.
      * @param cause   The cause of this exception.
+     *
+     * @author Lukas Hartwig
+     * @since 02.11.2021
      */
     public GameContainerException(String message, Throwable cause)
     {
